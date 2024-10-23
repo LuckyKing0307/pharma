5ce10573175ee8ece8c2e7d1f864f4df275fb587
LuckyKing0307 <jasaergasheva@gmail.com>
2024-10-23 10:22:38 +0500

    Regions

--- app/Exports/Sheets/Tablet.php
+++ app/Exports/Sheets/Tablet.php
@@ -147,13 +147,11 @@ class Tablet implements FromCollection, ShouldQueue, ShouldAutoSize, WithStyles,
             $file = UploadedFile::where(['file_id' => $tablet->uploaded_file_id]);
             if ($file->exists()){
                 $file = $file->get()->first();
+                if ($file->which_depo=='radez'){
+                    info('Массив');
+                    info($file->uploaded_date);
+                }
                 if ($file->uploaded_date){
-                    if ($file->which_depo=='radez'){
-                        info('Массив');
-                        info(Carbon::make($file->uploaded_date)->month);
-                        info('Массив11');
-                        info($tablet);
-                    }
                     $data[Carbon::make($file->uploaded_date)->month] += $tablet->sales_qty;
                     $price = str_replace(',', '.', $data['price']);
                     $data[Carbon::make($file->uploaded_date)->month+20] += intval($tablet->sales_qty)*$price;
