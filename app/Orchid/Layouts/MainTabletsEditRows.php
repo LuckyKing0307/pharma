587ffe99586481a618f3477b04ae624f3ee335a0
LuckyKing0307 <jasaergasheva@gmail.com>
2024-10-31 08:56:43 +0500

    Others

--- app/Orchid/Layouts/MainTabletsEditRows.php
+++ app/Orchid/Layouts/MainTabletsEditRows.php
@@ -33,28 +33,28 @@ class MainTabletsEditRows extends Rows
             Input::make('tablet.price')
                 ->title('Tablet Price')
                 ->placeholder('Enter price')->required(),
-            Relation::make('tablet.avromed')
+            Relation::make('tablet.avromed')->nullable(1)
                 ->fromModel(TabletMatrix::class, 'avromed', 'avromed')
                 ->title('Avromed'),
-            Relation::make('tablet.azerimed')
+            Relation::make('tablet.azerimed')->nullable(1)
                 ->fromModel(TabletMatrix::class, 'azerimed', 'azerimed')
                 ->title('Azerimed'),
-            Relation::make('tablet.aztt')
+            Relation::make('tablet.aztt')->nullable(1)
                 ->fromModel(TabletMatrix::class, 'aztt', 'aztt')
                 ->title('Aztt'),
-            Relation::make('tablet.epidbiomed')
+            Relation::make('tablet.epidbiomed')->nullable(1)
                 ->fromModel(TabletMatrix::class, 'epidbiomed', 'epidbiomed')
                 ->title('Epidbiomed')->value('tablet.epidbiomed'),
-            Relation::make('tablet.pasha-k')
+            Relation::make('tablet.pasha-k')->nullable(1)
                 ->fromModel(TabletMatrix::class, 'pasha-k', 'pasha-k')
                 ->title('Pasha-k'),
-            Relation::make('tablet.radez')
+            Relation::make('tablet.radez')->nullable(1)
                 ->fromModel(TabletMatrix::class, 'radez', 'radez')
                 ->title('Radez'),
-            Relation::make('tablet.sonar')
+            Relation::make('tablet.sonar')->nullable(1)
                 ->fromModel(TabletMatrix::class, 'sonar', 'sonar')
                 ->title('Sonar')->value('tablet.sonar'),
-            Relation::make('tablet.zeytun')
+            Relation::make('tablet.zeytun')->nullable(1)
                 ->fromModel(TabletMatrix::class, 'zeytun', 'zeytun')
                 ->title('Zeytun'),
         ];
