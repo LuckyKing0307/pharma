acd9461d7228602417dd980fb06f3e30805a6f5a
LuckyKing0307 <jasaergasheva@gmail.com>
2024-11-06 14:54:22 +0500

    Others

--- app/Imports/PashaImport.php
+++ app/Imports/PashaImport.php
@@ -12,7 +12,7 @@ use Maatwebsite\Excel\Concerns\WithBatchInserts;
 use Maatwebsite\Excel\Concerns\WithChunkReading;
 use Maatwebsite\Excel\Concerns\WithStartRow;
 
-class PashaImport implements ToModel, WithStartRow, WithChunkReading, WithBatchInserts, ShouldQueue
+class PashaImport implements ToModel, WithStartRow, WithChunkReading, WithBatchInserts
 {
 
     public string $firm = 'pasha-k';
