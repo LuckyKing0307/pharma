50c213ea6f56c97ef8414d1fdd6194a97d795875
LuckyKing0307 <jasaergasheva@gmail.com>
2024-12-06 16:10:38 +0500

    ExportBg

--- app/Orchid/Screens/ExportScreen.php
+++ app/Orchid/Screens/ExportScreen.php
@@ -49,8 +49,6 @@ class ExportScreen extends Screen
 
         return [
             Button::make('Create File')->method('download')->icon('plus'),
-            Link::make('Download')
-                ->href(env('APP_FILE_URL').'/export')->icon('download'),
         ];
     }
 
