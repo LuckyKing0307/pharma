e1abd5d770e455980b69d4fed013af1b91cf0bce
LuckyKing0307 <jasaergasheva@gmail.com>
2024-12-17 22:04:03 +0500

    ExportBg

--- app/Exports/DepoExport.php
+++ app/Exports/DepoExport.php
@@ -174,7 +174,7 @@ class DepoExport implements FromCollection, ShouldQueue, ShouldAutoSize, WithSty
         foreach ($tablets->get() as $tablet) {
             $file = UploadedFile::where(['file_id' => $tablet->uploaded_file_id]);
             if ($file->exists()){
-                dd($file->get());
+                dd($file->get()->first());
                 $file = $file->get()->first();
                 if ($file->uploaded_date){
                     $data[Carbon::make($file->uploaded_date)->month] += floatval($tablet->sales_qty);
